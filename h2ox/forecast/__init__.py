4c68409af446425ca24d7ec9fd868200adb4189a
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-02-20 15:43:42 +0000

    fix init and add ecmwfapi

--- h2ox/forecast/__init__.py
+++ h2ox/forecast/__init__.py
@@ -1,5 +1,5 @@
-from h2ox.forecast.tigge import download_tigge, ingest_tigge
+from h2ox.forecast.tigge import download_tigge, ingest_local_grib
 
 __version__ = "0.0.1"
 
-__all__ = ["__version__", "download_tigge", "ingest_tigge"]
+__all__ = ["__version__", "download_tigge", "ingest_local_grib"]
