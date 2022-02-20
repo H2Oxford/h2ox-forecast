1c83db824a08a5ea861bbdad5fdf33d65af82e09
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-02-20 13:37:48 +0000

    hardset version

--- h2ox/forecast/__init__.py
+++ h2ox/forecast/__init__.py
@@ -1,5 +1,5 @@
 from h2ox.forecast.tigge import download_tigge, ingest_tigge
 
-__version__ = 0.0.1
+__version__ = "0.0.1"
 
-__all__ = ["download_tigge", "ingest_tigge"]
+__all__ = ["__version__", "download_tigge", "ingest_tigge"]
